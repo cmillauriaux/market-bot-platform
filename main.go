d4f47514c42d898a529e310fb383c102a0c21a14
Cédric Millauriaux <cmillauriaux@icysoft.fr>
2017-08-11 14:38:12 +0200

    Ajout des logs au démarrage

--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
+	"log"
+
 	"github.com/cmillauriaux/market-bot-platform/engine"
 	"github.com/cmillauriaux/market-bot-platform/market"
 )
 
 func main() {
+	log.Println("Starting MBP")
 	market := market.InitMarket()
+	log.Println("Market initialized")
 	engine := engine.Init(market)
+	log.Println("Engine initialized")
+	log.Println("Loading history...")
 	engine.LoadHistory(".coinbaseEUR.csv")
+	log.Println("History loaded")
 	engine.ConnectToMarket()
+	log.Println("Connected to market")
+	log.Println("Launching supervision...")
 	engine.LaunchSupervision()
 }
